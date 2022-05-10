62869a7e0658bd214158f872ae45255fd3221cca
david <david.jaramillo323@yahoo.com>
2022-05-10 17:37:47 -0400

    'added-file-navigation'

--- python-test.py
+++ python-test.py
@@ -3,17 +3,28 @@ import os
 
 
 
+
 def folderSearcher():
+    fileArr = []    
     print(os.listdir())
     currDir = os.getcwd()
-    userInput = input('do you want to check files or folders: ')
+    userInput = input('do you want to check FILES, FOLDERS, or GO BACK: ')
     if(userInput == 'files'):
         typeCheck = input('which type of file do you want: ')
         for root, dirs, files in os.walk(currDir):
             for file in files: 
                 if file.endswith(typeCheck):
-                    print(file)
-    else:
+                    filePaths = os.path.abspath(file)
+                    fileArr += file
+                    print(fileArr)
+        #print(fileArr)
+
+    elif(userInput == 'go back'):
+        prev = os.path.normpath(currDir + os.sep + os.pardir)
+        os.chdir(prev)
+        return folderSearcher()
+
+    elif(userInput == 'folders'):
         folderInput = input('which folder do you want to check: ')
         #dirPath = os.path.dirname(currDir)
         print(currDir + '/' + folderInput)
@@ -21,9 +32,6 @@ def folderSearcher():
         os.chdir(currDir + '/' + folderInput)
         #print(os.listdir())
         return folderSearcher()
-    
-    
-    
     
 
 
