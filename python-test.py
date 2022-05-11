522a9485e3dd3a89d41b6dbbe1753f004afbfdaf
david <david.jaramillo323@yahoo.com>
2022-05-10 22:07:49 -0400

    'added-filtered-files-to-list'

--- python-test.py
+++ python-test.py
@@ -4,25 +4,32 @@ import os
 
 
 
-def folderSearcher():
-    fileArr = []    
+def folderSearcher(counter):  
+    fileArr = []  
     print(os.listdir())
     currDir = os.getcwd()
+    counter = 0
     userInput = input('do you want to check FILES, FOLDERS, or GO BACK: ')
     if(userInput == 'files'):
         typeCheck = input('which type of file do you want: ')
         for root, dirs, files in os.walk(currDir):
             for file in files: 
                 if file.endswith(typeCheck):
-                    filePaths = os.path.abspath(file)
-                    fileArr += file
+                    filePaths = os.path.join(root, file)
+                    #fileArr[counter] = fileArr[counter] + file
+                    fileArr = fileArr + [filePaths]
+                    counter += 1
+                    #print(counter)
+                    #print(filePaths)
                     print(fileArr)
-        #print(fileArr)
+        #for x in range(counter):
+            #fileArr = fileArr + ['']
+            #print(fileArr)
 
     elif(userInput == 'go back'):
         prev = os.path.normpath(currDir + os.sep + os.pardir)
         os.chdir(prev)
-        return folderSearcher()
+        return folderSearcher(0)
 
     elif(userInput == 'folders'):
         folderInput = input('which folder do you want to check: ')
@@ -31,10 +38,20 @@ def folderSearcher():
         #print(currDir)
         os.chdir(currDir + '/' + folderInput)
         #print(os.listdir())
-        return folderSearcher()
+        return folderSearcher(0)
     
 
 
-folderSearcher()
+folderSearcher(0)
 
+
+
+def arrGen():
+    empty = []
+    controller = 7
+    for x in range(controller):
+        print(x)
+
+
+#arrGen()
 
