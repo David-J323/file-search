bdf3c6a768e71b6afa20eeecc58e53f5c974a2ee
david <david.jaramillo323@yahoo.com>
2022-07-15 01:29:12 -0400

    'added-class-and-file-open-function'

--- python-test.py
+++ python-test.py
@@ -21,7 +21,8 @@ def folderSearcher(counter):
                     counter += 1
                     #print(counter)
                     #print(filePaths)
-                    print(fileArr)
+        print(fileArr)
+        open(fileArr[0], 'r')
         #for x in range(counter):
             #fileArr = fileArr + ['']
             #print(fileArr)
@@ -42,7 +43,7 @@ def folderSearcher(counter):
     
 
 
-folderSearcher(0)
+#folderSearcher(0)
 
 
 
@@ -54,4 +55,97 @@ def arrGen():
 
 
 #arrGen()
+
+
+
+class fileSearcher:
+    def __init__(self, instruct):
+        self.instruct = instruct
+        self.newArr = []
+        self.currFolder = os.getcwd() 
+        self.folder = 'folder'
+        self.file = 'file'
+        self.previousFolder = 'go back'
+
+    def generateCondition(self, conditionArg, conditionVal, trigger):
+        if(conditionArg == conditionVal):
+            trigger()
+        else: 
+            return
+
+    def openFileCondition(self, openArg, chosenFile, filePermission):
+        if(openArg == os.path.split(chosenFile)[-1]):
+            #print(chosenFile)
+            open(chosenFile, filePermission)
+            print('succcess!')
+        else: 
+            return
+
+
+    def search(self):
+        print(os.listdir())
+        userInput = input('Pick One of the Options to Continue - File, Folder, Go Back: ')
+        self.generateCondition(userInput, self.folder, self.folderSearch)
+        self.generateCondition(userInput, self.file, self.fileSearch)
+        self.generateCondition(userInput, self.previousFolder, self.goBack)
+
+        #if(userInput == 'folder'):
+         #   self.folderSearch()
+        #elif(userInput == 'file'):
+         #   self.fileSearch()
+        #elif(userInput == 'go back'):
+         #   self.goBack()
+
+    
+    def fileSearch(self):
+        typeCheck = input('which type of file do you want: ')
+        for root, dirs, files in os.walk(self.currFolder):
+            for file in files: 
+                if file.endswith(typeCheck):
+                    filePaths = os.path.join(root, file)
+                    #fileArr[counter] = fileArr[counter] + file
+                    self.newArr = self.newArr + [filePaths]
+                    #counter += 1
+                    #print(counter)
+                    #print(filePaths)
+        print(self.newArr)
+        self.fileOpener()
+
+
+    def fileOpener(self):
+        #print(os.path.split(self.newArr[0])[-1])
+        openInput = input("Which file do you want to open? ")
+        for x in self.newArr:
+            #print(x)
+            self.openFileCondition(openInput, x, 'r')
+
+    
+    def folderSearch(self):
+        print(os.listdir())
+        folderInput = input('Which folder in the directory do you want to search? ')
+        #self.newArr = self.newArr + ['yoloswagmaster69420']
+        #print(self.newArr)
+        self.currFolder = self.currFolder + '/' + folderInput
+        os.chdir(self.currFolder)
+        print(self.instruct + self.currFolder)
+        self.search()
+
+
+        #fileArr = fileArr + [self.searcher]
+        #print(fileArr)
+
+    
+    def goBack(self):
+        prev = os.path.normpath(self.currFolder + os.sep + os.pardir)
+        self.currFolder = prev
+        os.chdir(self.currFolder)
+        print('Went back to ----- ' + self.currFolder)
+        self.search()
+
+
+
+x = fileSearcher('The new directory is ----- ')
+x.search()
+
+
 
